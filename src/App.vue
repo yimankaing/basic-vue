d5f4a6d4355ab9da8f52a72fcdf0a9ac57705aa0
yimankaing <yimankaing@gmail.com>
2019-03-12 19:07:43 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Vue.js App"/>
+    <p v-if="!!result">result = {{result}}</p>
+    <button @click="calculate">calculate</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      value1: 10,
+      value2: 20,
+      result: 0
+    };
+  },
+  methods: {
+    calculate(){
+      this.result = this.value1 + this.value2
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
