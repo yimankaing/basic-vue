01e75c477e44554ba38bcbcf6e5ccfa5a2a4dce8
yimankaing <yimankaing@gmail.com>
2019-03-14 18:44:35 +0700

    add fn

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="app">
+  <div>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Vue.js App"/>
     <p v-if="!!result">result = {{result}}</p>
     <button @click="calculate">calculate</button>
+
+    <div>{{ message }}</div>
+
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import {sum} from './someFunction.js';
 export default {
-  name: "app",
+
   components: {
     HelloWorld
   },
   data() {
     return {
+      message: "hello message",
       value1: 10,
       value2: 20,
       result: 0
     };
   },
   methods: {
-    calculate(){
-      this.result = this.value1 + this.value2
+    calculate() {
+      this.result = sum(this.value1 , this.value2);
     }
   }
 };
---
+++ src/someFunction.js
@@ -0,0 +1,38 @@
+
+
+
+
+// export const sub = (a, b) => { 
+//     return a - b;
+// };
+
+// // function sub (){
+// //     return 
+// // }
+
+
+export const sum =(a, b)=>{
+    return a + b;
+};
+
+// // export default class Calculator {
+// //     static 
+// // }
+
+
+// const calculator =(a, b)=>{
+//     return a + b;
+// };
+// export default calculator;
+
+// export default class Cal {
+//     static sum =(a, b)=>{
+//         return a + b;
+//     };
+//     static sub = (a, b) => { 
+//         return a - b;
+//     };
+
+// }
+
+// export const white = "#000000";
